40abe8b0fa37a510d7583752e556e663223864e8
Juan Jesús Bonito Dueñas <jjbonitodu@msn.com>
2023-10-05 13:49:33 +0200

    [TheMoviesApp] Creating the application, all in the activity

--- app/src/main/java/com/juanje/themoviesapp/MainActivity.kt
+++ app/src/main/java/com/juanje/themoviesapp/MainActivity.kt
@@ -3,26 +3,59 @@ package com.juanje.themoviesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.juanje.themoviesapp.ui.theme.TheMoviesAppTheme
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TheMoviesAppTheme {
+
+                val movies = produceState<List<ServerMovie>>(initialValue = emptyList()) {
+                    value = Retrofit.Builder()
+                        .baseUrl("https://api.themoviedb.org/3/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build()
+                        .create(MovieService::class.java)
+                        .getMovies()
+                        .results
+                }
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(
+                        topBar = { TopAppBar(title = { Text(text = "Movies") }) }
+                    ) { padding ->
+                        LazyVerticalGrid(
+                            columns = GridCells.Adaptive(120.dp),
+                            modifier = Modifier.padding(padding),
+                            horizontalArrangement = Arrangement.spacedBy(4.dp),
+                            verticalArrangement = Arrangement.spacedBy(4.dp),
+                            contentPadding = PaddingValues(4.dp)
+                        ) {
+                            items(movies.value) { movie ->
+                                MovieItem(movie)
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -30,14 +63,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TheMoviesAppTheme {
-        Greeting("Android")
+private fun MovieItem(movie: ServerMovie) {
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colors.secondary)
+    ) {
+        AsyncImage(
+            model = "https://image.tmdb.org/t/p/w185/${movie.poster_path}",
+            contentDescription = movie.title,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(2 / 3f)
+        )
+        Text(
+            text = movie.title,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colors.background,
+            modifier = Modifier
+                .padding(4.dp)
+                .height(64.dp)
+                .align(Alignment.CenterHorizontally)
+        )
     }
 }
---
+++ app/src/main/java/com/juanje/themoviesapp/MovieService.kt
@@ -0,0 +1,8 @@
+package com.juanje.themoviesapp
+
+import retrofit2.http.GET
+
+interface MovieService {
+    @GET("discover/movie?api_key=d30e1f350220f9aad6c4110df385d380")
+    suspend fun getMovies(): MovieResult
+}
