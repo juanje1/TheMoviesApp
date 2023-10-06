75eed6b33fb9ec79fa73834cc8cc1edc9bc5b57c
Juan Jesús Bonito Dueñas <jjbonitodu@msn.com>
2023-10-06 14:22:38 +0200

    [TheMoviesApp] Creating the MVVM pattern

--- app/src/main/java/com/juanje/themoviesapp/MainActivity.kt
+++ app/src/main/java/com/juanje/themoviesapp/MainActivity.kt
@@ -4,21 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.produceState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.juanje.themoviesapp.ui.theme.TheMoviesAppTheme
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,15 +32,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             TheMoviesAppTheme {
 
-                val movies = produceState<List<ServerMovie>>(initialValue = emptyList()) {
-                    value = Retrofit.Builder()
-                        .baseUrl("https://api.themoviedb.org/3/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                        .create(MovieService::class.java)
-                        .getMovies()
-                        .results
-                }
+                val viewModel: MainViewModel = viewModel()
+                val state by viewModel.state.collectAsState()
 
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -44,15 +43,28 @@ class MainActivity : ComponentActivity() {
                     Scaffold(
                         topBar = { TopAppBar(title = { Text(text = "Movies") }) }
                     ) { padding ->
-                        LazyVerticalGrid(
-                            columns = GridCells.Adaptive(120.dp),
-                            modifier = Modifier.padding(padding),
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            verticalArrangement = Arrangement.spacedBy(4.dp),
-                            contentPadding = PaddingValues(4.dp)
-                        ) {
-                            items(movies.value) { movie ->
-                                MovieItem(movie)
+                        if(state.loading) {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator()
+                            }
+                        }
+                        if(state.movies.isNotEmpty()) {
+                            LazyVerticalGrid(
+                                columns = GridCells.Adaptive(120.dp),
+                                modifier = Modifier.padding(padding),
+                                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                                verticalArrangement = Arrangement.spacedBy(4.dp),
+                                contentPadding = PaddingValues(4.dp)
+                            ) {
+                                items(state.movies) { movie ->
+                                    MovieItem(
+                                        movie = movie,
+                                        onClick = { viewModel.onMovieClick(movie) }
+                                    )
+                                }
                             }
                         }
                     }
@@ -63,18 +75,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MovieItem(movie: ServerMovie) {
+private fun MovieItem(movie: ServerMovie, onClick: () -> Unit) {
     Column(
         modifier = Modifier
             .background(MaterialTheme.colors.secondary)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onDoubleTap = { onClick() }
+                )
+            }
     ) {
-        AsyncImage(
-            model = "https://image.tmdb.org/t/p/w185/${movie.poster_path}",
-            contentDescription = movie.title,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(2 / 3f)
-        )
+        Box {
+            AsyncImage(
+                model = "https://image.tmdb.org/t/p/w185/${movie.poster_path}",
+                contentDescription = movie.title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(2 / 3f)
+            )
+            if(movie.favourite) {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = movie.title,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(4.dp),
+                    tint = Color.Red
+                )
+            }
+        }
         Text(
             text = movie.title,
             textAlign = TextAlign.Center,
---
+++ app/src/main/java/com/juanje/themoviesapp/MainViewModel.kt
@@ -0,0 +1,43 @@
+package com.juanje.themoviesapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainViewModel : ViewModel() {
+
+    private val _state = MutableStateFlow(UiState())
+    val state: StateFlow<UiState> = _state
+
+    init {
+        viewModelScope.launch {
+            _state.value = UiState(loading = true)
+            _state.value = UiState(
+                movies = Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(MovieService::class.java)
+                .getMovies()
+                .results
+            )
+        }
+    }
+
+    fun onMovieClick(movie: ServerMovie) {
+        val movies = _state.value.movies.toMutableList()
+        movies.replaceAll {
+            if (it.id == movie.id) movie.copy(favourite = !it.favourite) else it
+        }
+        _state.value = _state.value.copy(movies = movies)
+    }
+
+    data class UiState(
+        val loading: Boolean = false,
+        val movies: List<ServerMovie> = emptyList()
+    )
+}
